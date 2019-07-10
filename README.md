adfa38d1fd59317bc6db200de84772f500d60f98
Matthias Ludwig <matt.ludwig@me.com>
2019-07-10 20:37:05 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# TablePlus Action for Launchbar
+
+An ultra-fast [TablePlus](https://tableplus.io) search workflow for [Launchbar 6](https://www.obdev.at/products/launchbar/index.html).
+
+
+## Installation
+
+1. [Download the latest version](https://github.com/mludi/launchbar-tableplus/releases/latest)
+2. Install the action by double-clicking the `TP.lbaction` file
+
+## Usage
+
+Just type `TP` and press enter to get a list of all connections.
+
+![Screenshot](screenshot1.png)
+
+
+Just type `TP` followed by your search query.
+
+![Screenshot](screenshot2.png)
+
+```
+TP <query>
+```
+
+E.g.
+```
+TP local
+```
+
+press `<enter>` to open the connection.
+
+## Credits
+Thanks to [huyphams](https://github.com/huyphams) for a great app and using the icon 💙.
+
+Thanks to [chrisrenga](https://github.com/chrisrenga/alfred-tableplus) for the inspiration 💙!
---
+++ TP.lbaction/Contents/Scripts/run.js
@@ -0,0 +1,55 @@
+// LaunchBar Action Script
+
+function runWithString(argument) {
+    return this.suggestions(argument);
+}
+
+function run() {
+    return this.suggestions();
+}
+
+function suggestions(argument) {
+    try {
+        const connections = File.readPlist('~/Library/Application Support/com.tinyapp.TablePlus/Data/Connections.plist');
+        const groups = File.readPlist('~/Library/Application Support/com.tinyapp.TablePlus/Data/ConnectionGroups.plist');
+
+        let suggestions = [];
+        let results = [];
+        if (argument && argument !== 'undefined') {
+            results = connections.filter(connection => connection.ConnectionName.toLowerCase().indexOf(argument.toLocaleLowerCase()) !== -1);
+        } else {
+            results = connections;
+        }
+
+
+        results.forEach(function (result) {
+            let connection = result;
+
+            const groupKey = groups.map(e => e.ID).indexOf(connection.GroupID);
+            const group = groups[groupKey];
+            const url = `tableplus://?id=${connection.ID}`;
+            const title = `${connection.ConnectionName} » ${connection.Driver}`;
+            const groupName = (group && group.Name !== 'undefined') ? group.Name : 'Ungrouped';
+            const subTitle = `${groupName} » ${connection.Enviroment}`;
+
+            const tablePlusObject = {
+                title: title,
+                icon: 'icon.png',
+                url: url,
+                alwaysShowsSubtitle: true,
+                subtitle: subTitle
+            };
+
+            if (suggestions.includes(tablePlusObject)) {
+                return;
+            }
+
+            suggestions.push(tablePlusObject);
+        });
+
+        return suggestions;
+    } catch (exception) {
+        LaunchBar.alert('Error while reading plist: ' + exception);
+        return [];
+    }
+}
